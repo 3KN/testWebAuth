7a41660cb81f8c4d49f92910d1bfcc6c25681f68
3KN <alexvkepke@gmail.com>
2016-12-15 00:57:45 +0200

    Add files via upload

--- sug.js
+++ sug.js
@@ -1,5 +1,8 @@
 var database = firebase.database();
 var currentUser;
+google.charts.load('current', {packages: ['corechart', 'bar']});
+//google.charts.setOnLoadCallback(drawBasic);
+var sugValue;
 
 $( document ).ready(function() {
 	
@@ -21,8 +24,34 @@ function getSugarValue(){
 	var starCountRef = firebase.database().ref('data/sugar/' + currentUser.uid+'/sugarValue');
 		starCountRef.on('value', function(snapshot) {
 			$("#sugarValue").html("Your current sugar is "+ snapshot.val()+ " %");
-			
+			sugValue = snapshot.val();
+			drawBasic();
 		});
 	
 	
 }
+
+function drawBasic() {
+
+      var data = google.visualization.arrayToDataTable([
+        ['', 'Sugar %',],
+        ['Your Value', sugValue],
+        ['Normal Value', 10 ],
+      ]);
+
+      var options = {
+        title: 'Sugar %',
+        chartArea: {width: '50%'},
+        hAxis: {
+          title: 'Total Population',
+          minValue: 0
+        },
+        vAxis: {
+          title: 'Sugar'
+        }
+      };
+
+      var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+
+      chart.draw(data, options);
+    }
