8b67e97c0c78fc04b33a5d7239b25215999a20b0
3KN <alexvkepke@gmail.com>
2016-12-15 00:13:34 +0200

    Add files via upload

--- newj.js
+++ newj.js
@@ -2,6 +2,8 @@ var database = firebase.database();
 var currentUser;
 google.charts.load('current', {packages: ['corechart', 'line']});
 google.charts.setOnLoadCallback(drawBasic);
+var dataArray = [];
+
 
 
 $( document ).ready(function() {
@@ -27,39 +29,44 @@ function getPulseValue(){
 	
 }
 
-
-
-function drawBasic() {
+function drawBasic(array1) {
+	
+	
 
       var data = new google.visualization.DataTable();
       data.addColumn('number', 'X');
-      data.addColumn('number', 'Dogs');
+      data.addColumn('number', 'Pulse Value');
 
-      data.addRows([
-        [0, 0],   [1, 10],  [2, 23],  [3, 17],  [4, 18],  [5, 9],
-        [6, 11],  [7, 27],  [8, 33],  [9, 40],  [10, 32], [11, 35],
-        [12, 30], [13, 40], [14, 42], [15, 47], [16, 44], [17, 48],
-        [18, 52], [19, 54], [20, 42], [21, 55], [22, 56], [23, 57],
-        [24, 60], [25, 50], [26, 52], [27, 51], [28, 49], [29, 53],
-        [30, 55], [31, 60], [32, 61], [33, 59], [34, 62], [35, 65],
-        [36, 62], [37, 58], [38, 55], [39, 61], [40, 64], [41, 65],
-        [42, 63], [43, 66], [44, 67], [45, 69], [46, 69], [47, 70],
-        [48, 72], [49, 68], [50, 66], [51, 65], [52, 67], [53, 70],
-        [54, 71], [55, 72], [56, 73], [57, 75], [58, 70], [59, 68],
-        [60, 64], [61, 60], [62, 65], [63, 67], [64, 68], [65, 69],
-        [66, 70], [67, 72], [68, 75], [69, 80]
-      ]);
+		for (var i = 0; i < array1.length; i++)
+		{
+		data.addRows([[i, array1[i]]])
+		
+		}
 
       var options = {
         hAxis: {
-          title: 'Time'
+          title: ''
         },
         vAxis: {
-          title: 'Popularity'
+          title: ''
         }
       };
 
       var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
 
       chart.draw(data, options);
+ }
+ 
+ function on dataChanges(){
+	 
+	 var ref = firebase.database().ref('data/pulseHistory/'+ currentUser.uid).limitToFirst(50);;
+		ref.once('value', function(snapshot) {
+			snapshot.forEach(function(childSnapshot) {
+				var childData = childSnapshot.val();
+				dataArray.push(childData);
+			});
+	drawBasic(dataArray)
+  
+});
+	 
  }
