ac7b0d9cee163b31d7386ad9da53f25709383849
3KN <alexvkepke@gmail.com>
2016-12-14 22:59:14 +0200

    Add files via upload

---
+++ newj.js
@@ -0,0 +1,26 @@
+var database = firebase.database();
+var currentUser;
+
+
+$( document ).ready(function() {
+	
+	
+	firebase.auth().onAuthStateChanged(function(user) {
+	if (user) {
+		currentUser = user;
+		getPulseValue();
+	} else {
+			$("#PulseValue").html("Sign in First");
+	});
+	
+
+});
+
+function getPulseValue(){
+	var starCountRef = firebase.database().ref('data/pulse/' + currentUser.uid+'/pulseValue');
+		starCountRef.on('value', function(snapshot) {
+			$("#PulseValue").html("Your current pulse  "+ snapshot.val());
+		});
+	
+	
+}
