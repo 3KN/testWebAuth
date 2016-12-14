67473cd6956f674638196112d84c395b90946329
3KN <alexvkepke@gmail.com>
2016-12-15 00:51:13 +0200

    Add files via upload

--- sug.js
+++ sug.js
@@ -7,10 +7,22 @@ $( document ).ready(function() {
 	firebase.auth().onAuthStateChanged(function(user) {
 	if (user) {
 		currentUser = user;
-		
+		getSugarValue();
 	} else {
 			$("#sugarValue").html("Sign in First");
 	}
 	});
 
 });
+
+
+
+function getSugarValue(){
+	var starCountRef = firebase.database().ref('data/sugar/' + currentUser.uid+'/sugarValue');
+		starCountRef.on('value', function(snapshot) {
+			$("#sugarValue").html("Your current sugar is "+ snapshot.val()+ " %");
+			
+		});
+	
+	
+}
